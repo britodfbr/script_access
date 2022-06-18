e898cb9307ef075473657cb3f24ce7db07f972b2
britodfbr <>
2022-06-18 11:35:13 +0000

    refactor: Correção da função fake

--- incolumepy/incolume_cli/click/mine.py
+++ incolumepy/incolume_cli/click/mine.py
@@ -1,6 +1,7 @@
 import click
 from incolumepy.others.xpto import gen_model_conf
 
+
 @click.group()
 @click.pass_context
 def infosaj():
@@ -8,13 +9,15 @@ def infosaj():
 
 
 @click.command()
-@click.option('-f', "--fileconf", prompt="Your file config name", help="Provide your file config name")
+@click.option('-f',
+              "--fileconf",
+              prompt="Your file config name",
+              help="Provide your file config name")
 def gen_model(fileconf):
     gen_model_conf(fileconf)
 
 
 infosaj.add_command(gen_model, 'genmodel')
-
 
 if __name__ == '__main__':
     gen_model_conf()
--- incolumepy/others/xpto.py
+++ incolumepy/others/xpto.py
@@ -1,2 +1,7 @@
-def gen_model_conf():
-    return 'model generate!'
+from pathlib import Path
+
+
+def gen_model_conf(filename=None):
+    fout = Path(filename) if filename else Path('tmp/model.yml')
+    fout.write_bytes(b'model generate!')
+    return True
