3fac3c1c848664516c246f800dc87bef28651248
britodfbr <>
2022-06-19 01:19:02 +0000

    refactor: tratativa em passagem de contexto

--- incolumepy/incolume_cli/click/mine.py
+++ incolumepy/incolume_cli/click/mine.py
@@ -1,6 +1,6 @@
 import click
 from incolumepy.others.xpto import gen_model_conf
-
+import inspect
 
 @click.group()
 @click.pass_context
@@ -8,16 +8,28 @@ def infosaj():
     pass
 
 
-@click.command()
+@infosaj.command()
 @click.option('-f',
               "--fileconf",
               prompt="Your file config name",
               help="Provide your file config name")
 def gen_model(fileconf):
+    click.secho(f'{inspect.stack()[0][3]}', fg='red')
     gen_model_conf(fileconf)
 
 
+@infosaj.command()
+@click.option('-f',
+              "--fileconf",
+              prompt="Your file config name",
+              help="Provide your file config name")
+def gen_infosaj(fileconf, fout):
+    click.secho(f'{inspect.stack()[0][3]}', fg='red')
+    gen_model_conf(fileconf)
+
 infosaj.add_command(gen_model, 'genmodel')
+infosaj.add_command(gen_infosaj, 'geninfo')
+
 
 if __name__ == '__main__':
     gen_model_conf()
